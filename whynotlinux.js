48919e9b72d63f2b7be4804d05040b1468f95586
lahatra3 <lahatrap20.aps2a@gmail.com>
2023-04-16 18:40:02 +0300

    [ADD] Date of whynotlinux and chalk dependancies...

--- whynotlinux.js
+++ whynotlinux.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import chalkAnimation from "chalk-animation";
+import chalk from "chalk";
 import figlet from "figlet";
 
 const distributions = [
@@ -16,26 +17,33 @@ const distributions = [
     "Suse",
     "ParrotOS",
   ],
-  distributions_length = distributions.length;
+  distributionsLength = distributions.length;
 
 const startRainbowLinux = async () => {
     const rainbowLinux = chalkAnimation.rainbow(
       figlet.textSync(`Why  not  ${distributions[0]} ?`)
     );
-    return new Promise((resolve) => rainbowLinux.start(() => resolve()));
+    return new Promise(resolve => rainbowLinux.start(() => resolve()));
+  },
+  displayDate = async () => {
+    const position = "\n=============>>> ESTI Antanimena, Samedi 29 Avril 2023 <<<=============\n";
+    return new Promise(resolve => {
+      console.log(chalk.hex('#008080').bold(position));
+      resolve();
+    });
   },
   startRainbowDistribution = async () => {
     const rainbowDistribution = chalkAnimation.rainbow(
-      `Why not ${distributions[0]} ?`
+      `\t\t\tWhy not ${distributions[0]} ?`
     );
     return new Promise((resolve) => {
       setInterval(() => {
-        let index = Math.round(Math.random() * (distributions_length - 1));
-        rainbowDistribution.replace(`Why not ${distributions[index]} ?`);
+        let index = Math.round(Math.random() * (distributionsLength - 1));
+        rainbowDistribution.replace(`\t\t\tWhy not ${distributions[index]} ?`);
         resolve();
       }, 1331);
     });
   };
 
 (async () =>
-  await Promise.all([startRainbowLinux(), startRainbowDistribution()]))();
+  await Promise.race([startRainbowLinux(), displayDate(), startRainbowDistribution()]))();
